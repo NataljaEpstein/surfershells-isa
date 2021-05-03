1b0df053b0294d5f5da02438e493ea182cd3d8fc
Natalja Epstein <natalja.epstein@accenture.com>
2021-05-03 12:08:13 +0200

    second commit

---
+++ src/main/java/com/accenture/surfershells/International_Stock_Analysis/App.java
@@ -0,0 +1,13 @@
+package com.accenture.surfershells.International_Stock_Analysis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
